d4af670cf1c838e8710a7764dc6b8833dff48af8
nymulislam <naymulislam241@gmail.com>
2023-12-12 01:23:27 +0600

    Added Home Section.

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import "./home.css";
+import Social from "./Social";
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home_container container grid">
+        <div className="home_content gird">
+          <Social />
+
+          <div className="home_img"></div>
+
+          <Data />
+        </div>
+
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,21 @@
+
+const Social = () => {
+  return (
+    <div className="home_social">
+
+      <a href="" className="home_social-icon" target="_black">
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a href="" className="home_social-icon" target="_black">
+        <i className="uil uil-dribbble"></i>
+      </a>
+
+      <a href="" className="home_social-icon" target="_black">
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
